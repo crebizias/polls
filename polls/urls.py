ec3b5b0f916424f92ef51f2759362f2124d81a22
crabizias <crbizias@naver.com>
2021-11-25 08:38:04 +0900

    test

---
+++ polls/urls.py
@@ -0,0 +1,20 @@
+from . import views
+from django.urls import path
+from django.http import HttpResponse
+from .models import Question
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+# Leave the rest of the views (detail, results, vote) unchanged
+
+app_name = 'polls'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
